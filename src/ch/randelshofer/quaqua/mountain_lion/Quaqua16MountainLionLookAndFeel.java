b854e96b8ab60a01dc075b3a9e01351cd1184f5a
Karl von Randow <karl@cactuslab.com>
2013-07-31 16:48:11 +1200

    Toolbar borders: fix for color match on Java 1.6 vs 1.7. Allow toolbar title border to use InactivatableColorUIResource.

--- src/ch/randelshofer/quaqua/mountain_lion/Quaqua16MountainLionLookAndFeel.java
+++ src/ch/randelshofer/quaqua/mountain_lion/Quaqua16MountainLionLookAndFeel.java
@@ -3,6 +3,8 @@ package ch.randelshofer.quaqua.mountain_lion;
 import javax.swing.UIDefaults;
 import javax.swing.plaf.ColorUIResource;
 
+import ch.randelshofer.quaqua.QuaquaManager;
+import ch.randelshofer.quaqua.color.InactivatableColorUIResource;
 import ch.randelshofer.quaqua.lion.Quaqua16LionLookAndFeel;
 
 public class Quaqua16MountainLionLookAndFeel extends Quaqua16LionLookAndFeel {
@@ -19,13 +21,18 @@ public class Quaqua16MountainLionLookAndFeel extends Quaqua16LionLookAndFeel {
 	protected void initDesignDefaults(UIDefaults table) {
 		super.initDesignDefaults(table);
 		
-        Object toolBarBackground = new ColorUIResource(222, 222, 222);
-        
-		Object[] uiDefaults = new Object[] {
-			"ToolBar.title.background", toolBarBackground
-		};
-		
-		putDefaults(table, uiDefaults);
+		String javaVersion = QuaquaManager.getProperty("java.version", "");
+		if (javaVersion.startsWith("1.5") || javaVersion.startsWith("1.6")) {
+			/* No need to do anything on Java 1.6 or earlier. */
+		} else {
+	        Object toolBarBackground = new InactivatableColorUIResource(new ColorUIResource(222, 222, 222), new ColorUIResource(246, 246, 246));
+	        
+			Object[] uiDefaults = new Object[] {
+				"ToolBar.title.background", toolBarBackground
+			};
+			
+			putDefaults(table, uiDefaults);
+		}
 	}
 
 }
